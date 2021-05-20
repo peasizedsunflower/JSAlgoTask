ad36a408ccaa54543d2753a7c052b988b131340d
peasizedsunflower <mtlabiola@gmail.com>
2021-05-19 21:58:09 -0600

    bug fix

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
 //SECTION ONE
-//EVENT LISTENERS
-eventListeners();
 
-function eventListeners(){
-    //submit
-    document.querySelector('#formOne').addEventListener('submit', convertFahrToCelsius);
-    //second submit
-    document.querySelector('#formTwo').addEventListener('submit', checkYuGiOh);
-};
+convertFahrToCelsius(0);
 
-//FUNCTIONS
-function convertFahrToCelsius(e){
-    e.preventDefault();
-    const fahr = document.getElementById('parameter').value,
-        // fahr = Number(f);
-         convert = (fahr - 32) * 5/9,
-         convertRU = convert.toFixed(4);
-    const c = document.getElementById('answer');
-    c.value = convertRU;
-    console.log(fahr);
-    console.log(typeof fahr);
-    if(typeof fahr !== number){
-        c.value = `${fahr} is not a valid number but a/an ${typeof fahr}`;
+function convertFahrToCelsius(fahr){
+    let convert = document.getElementById('convert');
+    if(typeof fahr !== 'number'){
+        convert.value = `${fahr} is not a valid number but a/an ${typeof fahr}`;
+    } else {
+        let celsius = (fahr - 32) * 5/9;
+        celsius = celsius.toFixed(4);
+        convert.value = `${celsius} deg C`;
     };
 };
 
-function checkYuGiOh(e){
-    e.preventDefault();
-    const n = document.getElementById('number').value;
-    console.log(n);
-    console.log(typeof n);
-}
+//SECTION TWO
+checkYuGiOh(10);
+
+function checkYuGiOh(value){
+    
+};
